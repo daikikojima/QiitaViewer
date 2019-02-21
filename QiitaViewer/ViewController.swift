d9b5d3769473a56741d7d5de8acf54d4d95762e4
daikikojima <daikikojima0531@icloud.com>
2019-02-21 13:36:17 +0900

    Initial Commit

---
+++ QiitaViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QiitaViewer
+//
+//  Created by Daiki Kojima on 2019/02/21.
+//  Copyright © 2019 Daiki Kojima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
